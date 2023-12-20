5040eb555f68ea448f19d58c487bda2e732f0c39
cebisli <ikramcebisli@hotmail.com>
2023-12-20 11:51:15 +0300

    common.js te değişiklik yapıldı.

--- public/js/common.js
+++ public/js/common.js
@@ -1,24 +1,5 @@
-
-const Modal_Small = 1;
-const Modal_Large = 2;
-const Modal_XLarge = 3;
-const Modal_FullScreen = 3;
-const Modal_DialogScrollable = 5;
-
-const ShowBSDialog = (divId, callback, tur = 0) =>
+const ShowBSDialog = (divId, callback, tur = 1) =>
 {
-	var modalClass = "";
-	if (tur == Modal_Small)
-		modalClass = "modal-sm";
-	else if (tur == Modal_Large)
-		modalClass = "modal-lg";
-	else if (tur == Modal_XLarge)
-		modalClass = "modal-xl";
-	else if (tur == Modal_FullScreen)
-		modalClass = "modal-fullscreen";
-	else if (tur == Modal_DialogScrollable)
-		modalClass = "modal-dialog-scrollable";
-
 	var div = $('#' + divId).show();
 	var title = $('#' + divId).attr('title');
 	var modalId = divId + '_modal';
@@ -26,7 +7,7 @@ const ShowBSDialog = (divId, callback, tur = 0) =>
 	modalWrap = document.createElement('div');
 	modalWrap.innerHTML = `
 	  <div class="modal fade" tabindex="-1" id="`+modalId+`" data-backdrop="static" data-keyboard="false">
-		<div class="modal-dialog `+modalClass+`">
+		<div class="modal-dialog" style="max-width:750px; height:50%;">
 		  <div class="modal-content">
 			<div class="modal-header bg-light">
 			  <h5 class="modal-title">${title}</h5>
